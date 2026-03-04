88d78e8a2067b8b47b474566a6c83802535d96fc
cpt_rogers <alireza.firouzja098@gmail.com>
2026-03-04 09:51:06 +0530

    4-3-26

---
+++ sorted.py
@@ -0,0 +1,9 @@
+def is_sorted(userlist):
+    new_list = sorted(userlist)
+    if new_list == userlist:
+        return True
+    else:
+        return False
+
+userlist=[int(n) for n in input("input a list of numbers:").split()]
+print(is_sorted(userlist))
